bb514a6c60b4bd1a003d46bfecbe0690cac6a49e
Gustavo Mendes <gustavoomendesoliveira@gmail.com>
2020-12-03 01:36:42 -0300

    Adicionando layout base de login e chamada da Home

--- src/pages/Home/HomeComponent.vue
+++ src/pages/Home/HomeComponent.vue
@@ -4,10 +4,21 @@
 
 <script>
 export default {
-    
+    name: 'LoginComponent',
+    data(){
+        return{
+            
+        }
+    },
+    methods: {
+        login(){
+            this.$router.push({name:'home'});
+        }
+    }
 }
 </script>
 
 <style scoped>
 
 </style>
+
--- src/pages/Login/LoginComponent.vue
+++ src/pages/Login/LoginComponent.vue
@@ -3,15 +3,20 @@
     <div class="main">
         <div class="login">
             <div class="content-login">
-                <header>
-                    <h1>Login</h1>
-                    <b-row>
-                        <b-form-group label="Usuário" label-for="user-name">
-
-                            
-                        </b-form-group>
-                    </b-row>  
-                </header>
+                <form>
+                    <div class="login-body">
+                   
+                        <h2 >Login</h2>
+                        <input  type="text"  placeholder="Usuário" class="input-text">
+                        <h1></h1>
+                        <input type="password" placeholder="Senha"  class="input-text" >
+                        <h1></h1>
+                        <a href='' target=''>Recuperar senha.</a><br/>
+                        <button type="submit" id="Login" class="buttons" @click.prevent="login">Login</button>
+                        <h1></h1>
+                        <button type="submit" id="Cadastro" class="buttons" >Cadastre-se</button>
+                    </div>
+                </form>    
             </div>
         </div>
     </div>
@@ -20,7 +25,17 @@
 
 <script>
 export default {
-    
+    name: 'LoginComponent',
+    data(){
+        return{
+            
+        }
+    },
+    methods: {
+        login(){
+            this.$router.push({name:'home'});
+        }
+    }
 }
 </script>
 
