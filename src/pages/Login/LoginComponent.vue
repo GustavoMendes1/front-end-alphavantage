66ba900fe6c5df09214fe6d9b8810cfd9c25d4b0
Gustavo Mendes <gustavoomendesoliveira@gmail.com>
2020-12-02 23:26:27 -0300

    Adicionando estrutura básica do projeto

---
+++ src/pages/Login/LoginComponent.vue
@@ -0,0 +1,29 @@
+<template>
+
+    <div class="main">
+        <div class="login">
+            <div class="content-login">
+                <header>
+                    <h1>Login</h1>
+                    <b-row>
+                        <b-form-group label="Usuário" label-for="user-name">
+
+                            
+                        </b-form-group>
+                    </b-row>  
+                </header>
+            </div>
+        </div>
+    </div>
+
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" src="./style.scss" scoped>
+
+</style>
