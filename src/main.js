a16cb7329754d03b397f4ebaf0573a6f674e2105
Gustavo Mendes <gustavoomendesoliveira@gmail.com>
2020-12-04 14:05:57 -0300

    Adicionando estrutura básica de gráficos

--- src/main.js
+++ src/main.js
@@ -3,18 +3,21 @@ import App from './App.vue'
 import VueRouter from 'vue-router'
 import LoginComponent from './pages/Login/LoginComponent'
 import HomeComponent from './pages/Home/HomeComponent'
+import VueGoogleCharts from 'vue-google-charts'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 
 Vue.use(VueAxios, axios)
 Vue.use(VueRouter)
+Vue.use(VueGoogleCharts)
 Vue.config.productionTip = false
 
 
 const routes = [
   {path:'/', name:'login', component: LoginComponent},
   {path:'/home', name: 'home',component: HomeComponent},
+
 ]
 
 const router = new VueRouter({
--- src/pages/Home/HomeComponent.vue
+++ src/pages/Home/HomeComponent.vue
@@ -1,20 +1,60 @@
 <template>
+    <div>
+    <div class="barra"></div>
     <p>Home</p>
+     <GChart
+        type="AreaChart"
+        :data="chartData"
+        :options="chartOptions"
+    />
+</div>
+    
 </template>
 
+<script src="vue.js"></script>
+<script src="vue-google-charts/dist/vue-google-charts.browser.js"></script>
 <script>
 
 const axios = require('axios');
 
+import { GChart } from 'vue-google-charts'
+
+
 export default {
     name: 'HomeComponent',
+    components: {
+        GChart
+    },
     data(){
         return{
+        chartData: [
             
+        ['Hours', 'IBM'],
+        ['16:10', 123.6224],
+        ['16:15', 124.0000],
+        ['16:20', 124.0000],
+        ['16:30', 124.0000],
+        ['16:35', 123.6100],
+        ['16:40', 123.6300],
+        ['16:55\n 12/03', 123.6100],
+        ['17:30', 124.1600],
+        ['18:15', 123.9300],
+        ['18:35', 123.7100],
+       
+
+        ],
+         chartOptions: {
+        chart: {
+          title: 'Company Performance',
+          subtitle: 'Sales, Expenses, and Profit: 2014-2017',
+        }
+      }
         }
     },
     mounted () {
         this.getValues();
+       
+        
     },
     methods: {
         getValues () {
@@ -24,14 +64,12 @@ export default {
             }).catch(err => {
                     console.log(err.response);
             });
-
         }
     }
-   
 }
 </script>
 
-<style scoped>
+<style lang="scss" src="./style.scss" scoped>
 
 </style>
 
